cf08c6684487ec1c0b418a126733dc0c6a964f38
Matt Porter <mattyp.ryan@gmail.com>
2023-05-24 18:56:44 -0700

    Matts stuff added

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,54 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
-import{useNavigate,Link} from 'react-router-dom'
-const Register = (props)=>{
+import { useNavigate, Link } from 'react-router-dom'
+const Register = (props) => {
     const navigate = useNavigate()
-    const[user,setUser]=useState({
-        userName:'',
-        userLast:'',
-        email:'',
-        password:'',
-        confirmPassword:''
+    const [user, setUser] = useState({
+        userName: '',
+        userLast: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
     })
     const changeHandler = (e) => {
-        setUser({...user,[e.target.name]:e.target.value})
+        setUser({ ...user, [e.target.name]: e.target.value })
     }
 
     //Submit Handler
-    const submitHandler = (e)=>{
+    const submitHandler = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:8080/api/register',user,{withCredentials:true})
-            .then((res)=>{
+        axios.post('http://localhost:8000/api/register', user, { withCredentials: true })
+            .then((res) => {
                 console.log(res);
                 navigate('/dashboard')
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err);
             })
     }
 
-    return(
+    return (
         <div>
             <form onSubmit={submitHandler}>
                 <div>
                     <label>First Name:</label>
-                    <input type ='text' onChange={changeHandler} value={user.userName} name='userName'></input>
+                    <input type='text' onChange={changeHandler} value={user.userName} name='userName'></input>
                 </div>
                 <div>
                     <label>Last Name:</label>
-                    <input type ='text' onChange={changeHandler} value={user.userLast} name='userLast'></input>
+                    <input type='text' onChange={changeHandler} value={user.userLast} name='userLast'></input>
                 </div>
                 <div>
                     <label>Email:</label>
-                    <input type ='text' onChange={changeHandler} value={user.email} name='email'></input>
+                    <input type='text' onChange={changeHandler} value={user.email} name='email'></input>
                 </div>
                 <div>
                     <label>Password:</label>
-                    <input type ='password' onChange={changeHandler} value={user.password} name='password'></input>
+                    <input type='password' onChange={changeHandler} value={user.password} name='password'></input>
                 </div>
                 <div>
                     <label>Confirm Password:</label>
-                    <input type ='password' onChange={changeHandler} value={user.confirmPassword} name='confirmPassword'></input>
+                    <input type='password' onChange={changeHandler} value={user.confirmPassword} name='confirmPassword'></input>
                 </div>
                 <button>Register</button>
             </form>
--- server/models/breakModel.js
+++ server/models/breakModel.js
@@ -6,13 +6,23 @@ const BreakSchema = new mongoose.Schema({
         required: [true, 'Break name is required'],
         minLength: [3, 'Break name must be 3 or more characters']
     },
-    description: {
-        type: Object
+    temperature: {
+        type: Number,
+        required: [true, "Temperature is required!"]
+    },
+    waveHeight: {
+        type: Number,
+        required: [true, "Wave height is required!"]
+    },
+    windDirection: {
+        type: String,
+        required: [true, "Wind Direction is required!"]
     },
     notes: {
         type: String,
+        required: [false]
     }
-}, {timestamps: true});
+}, { timestamps: true });
 
 const Break = mongoose.model('Break', BreakSchema)
 module.exports = Break
